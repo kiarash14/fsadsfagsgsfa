c5148622d465e3fa59ca531b59492923eae34e9d
aaabanana <navidspamkiller@gmail.com>
2016-05-17 07:08:35 -0700

    Update wai.lua

--- plugins/wai.lua
+++ plugins/wai.lua
@@ -4,24 +4,24 @@ local function run(msg, matches)
   if matches[1] == 'wai' then
     if is_sudo(msg) then
      --send_document(get_receiver(msg), "/home/Seed/axs/sudo.webp", ok_cb, false)
-      return "You are my father"
+      return "😍 بابا بیا بغلم 😍"
     elseif is_owner(msg) then
    -- send_document(get_receiver(msg), "/home/Seed/axs/owner.webp", ok_cb, false)
-      return "You are Group Owner"
+      return "شما صاحب گروه هستی"
     elseif is_momod(msg) then
   --  send_document(get_receiver(msg), "/home/Seed/axs/mod.webp", ok_cb, false)
-      return "You are Group Mod"
+      return "شما یک کمک مدیر هستی"
     else
   --  send_document(get_receiver(msg), "/root/Tele/axs/mmbr.webp", ok_cb, false)
-      return "You are Group Member"
+      return "شما یک عضو معمولی هستید"
     end
   end
 end
 
 return {
   patterns = {
-    "^[#!/]([Ww]ai)$",
-     "^([Ww]ai)$"
+    "^(من)$",
+     "^(me)$"
     },
   run = run
 }
