224cfa74b8ede3644f4a300e60e1ac813896677b
aaabanana <navidspamkiller@gmail.com>
2016-05-17 07:33:03 -0700

    Update voice.lua

--- plugins/voice.lua
+++ plugins/voice.lua
@@ -1,20 +1,19 @@
-do
 local function run(msg, matches)
-  local url = "http://www.farsireader.com/PlayText.aspx?text="..matches[1]
+  local eq = matches[1]
+
+  local url = "http://www.farsireader.com/PlayText.aspx?Text="..eq.."Punc=false"
   local receiver = get_receiver(msg)
   local file = download_to_file(url,'text.ogg')
-      send_audio('channel#id'..msg.to.id, file, ok_cb , false)
+  send_audio('channel#id'..msg.to.id, file, ok_cb , false)
 end
 
 return {
-  description = "text to voice",
+  description = "تبدیل به صدا",
   usage = {
-    "!voice [text]"
+    "بخوان : خواندن یک متن"
   },
   patterns = {
-    "^[!/#]voice (.+)$"
+    "^(بخوان) (.+)$"
   },
   run = run
 }
-
-end
